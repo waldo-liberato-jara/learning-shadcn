30d480194231a9b9b37b3e45eefd1c0e39bb0d05
waldo-liberato-jara <waldo.liberato@opolarity.com>
2026-02-10 10:23:19 -0500

    deploy: crud v1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/waldo-liberato-jara/",
+  base: "/learning-shadcn/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
